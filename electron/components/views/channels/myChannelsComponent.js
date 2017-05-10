a50a99ecb3e697bf018ee0bd968e5b51dae8fce4
Ricardo Ferreira da Silva <tansvanio@hotmail.com>
2017-05-10 13:19:16 +0100

    Getting chat

--- electron/components/views/channels/myChannelsComponent.js
+++ electron/components/views/channels/myChannelsComponent.js
@@ -12,16 +12,54 @@ import {
 class MyChannelsComponent extends React.Component {
   constructor(props)
   {
-    super();
+      super();
+      this.state = 
+      {
+          chats: []
+      }
+      this.getChats = this.getChats.bind(this);
   }
+  componentDidMount() {
+      this.getChats();
+  }
+  getChats()
+  {
+      console.log("Getting chats.");
+      var data =
+      {
+              "token": localStorage.getItem("token"),
+      };
+      var request = new XMLHttpRequest();
+      request.open('POST', 'http://vps301278.ovh.net:3561/getChats');
+      request.setRequestHeader("Content-type", "application/json");
+      request.onreadystatechange = () => {
+          if (request.readyState !== 4) {
+              return;
+          }
+          if (request.status === 200) {
+              var res = JSON.parse(request.responseText);
+              this.setState({chats:res});
+              console.log(this.state.chats);
+          } else {
+              console.warn('error');
+          }
+      };
+      request.send(JSON.stringify(data));
+  }
+  drawChats() 
+  {
+    var listChats = this.state.chats.map((chat) =>
+        <ListGroupItem header={chat.chat_name} key={chat.id}>
+            <Button bsStyle="success" value={chat.id}>Join</Button>
+        </ListGroupItem>);
+    return (<ListGroup>{listChats}</ListGroup>);
+ }
   render() {
     return (
       <div>
         <h1>My Channels</h1>
         <ListGroup>
-            <ListGroupItem header="SDIS" href="#">SDIS channel</ListGroupItem>
-            <ListGroupItem header="LBAW" href="#">LBAW channel</ListGroupItem>
-            <ListGroupItem header="COMP" href="#">COMP channel</ListGroupItem>  
+           {this.drawChats()}
         </ListGroup>
       </div>
     )
